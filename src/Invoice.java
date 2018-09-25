0f8d56fc43814d5cdea404d7257b0763d5093c7a
Gabriel Walsh <g.b.j.walsh@gmail.com>
2018-09-25 10:28:21 -0230

    added to invoice class sept 25 2018

--- src/Invoice.java
+++ src/Invoice.java
@@ -3,4 +3,54 @@
  * an invoice for a hardware store
  */
 public class Invoice {
+    public String partNUM;
+    public String pDiscription;
+    public int quantity;
+    public double price;
+    public String set
+
+    /**
+     * get method for price
+     * @return double
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * get method for the number of items bought
+     * @return int
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * get method for the discription of the part
+     * @return string
+     */
+
+    public String getpDiscription() {
+        return pDiscription;
+    }
+
+    /**
+     * get method for part number
+     * @return string
+     */
+
+    public String getPartNUM() {
+        return partNUM;
+    }
+    public void balance(){
+        price = getPrice();
+        quantity = getQuantity();
+        total = price*quantity;
+        if (total<0){
+            return 0;
+        }
+        else{
+            return total;
+        }
+    }
 }
