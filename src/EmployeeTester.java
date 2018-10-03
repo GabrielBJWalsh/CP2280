edd0ecbb5707b6fd2e3c42f3d41342a648774938
gbw767 <31829675+gbw767@users.noreply.github.com>
2018-10-03 20:09:04 -0230

    re did employee class should be ready for demo oct 3 2018

--- src/EmployeeTester.java
+++ src/EmployeeTester.java
@@ -1,49 +1,23 @@
 /**
- * tester class for empyloee
- *
+ * tester class for Employee
  */
 public class EmployeeTester {
     public static void main(String[] args) {
         Employee employee = new Employee("David", "Parsons", 3900.00);
         Employee employee2 = new Employee("Dave", "par", 3000.00);
-        System.out.println("heres an employee:");
+        employee.printEmp(employee);
+        employee.yearly(employee);
+        employee2.printEmp(employee2);
+        employee2.yearly(employee2);
 
+        System.out.println("\n\nyay Dave Par got a 10% raise");
 
-        System.out.printf("name: %s %s\nMothly Salary: %.2f\n",
-                employee.getFname(),
-                employee.getLname(),
-                employee.getSalary()
-        );
-        System.out.printf("yearly Salary: %.2f\n",
-                employee.getSalary()*12);
-
-        System.out.println("here is another employee:");
-        System.out.printf("name: %s %s\nMonthlySalary: %.2f\n",
-                employee2.getFname(),
-                employee2.getLname(),
-                employee2.getSalary()
-        );
-        System.out.printf("yearly Salary: %.2f\n",
-                employee.getSalary()*12);
-        System.out.println("yay David got a 10% raise");
-        double emp = employee.getSalary();
-        double pme = employee.setSalary(emp)*1.1;
-        System.out.println(pme);
-        employee.setSalary(pme);
-        System.out.printf("name: %s %s\nMothly Salary: %.2f\n",
-                employee.getFname(),
-                employee.getLname(),
-                employee.getSalary()
-        );
-        System.out.printf("yearly Salary: %.2f\n",
-                employee.getSalary()*12);
-
-
-
+        employee2.giveRaise(employee2);
+        employee2.printEmp(employee2);
+        employee2.yearly(employee2);
 
     }
 
 
-
-
 }
+
