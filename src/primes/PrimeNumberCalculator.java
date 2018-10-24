fd327501bc8ef6a63c43a2f26ac128af59f53bf9
Gabriel Walsh <g.b.j.walsh@gmail.com>
2018-10-24 16:32:46 -0230

    updated age method in Patient class oct 23

--- src/primes/PrimeNumberCalculator.java
+++ src/primes/PrimeNumberCalculator.java
@@ -1,2 +1,35 @@
+package primes;
+/**
+ * prime calculator class
+ * @author Gabe Walsh
+ */
 public class PrimeNumberCalculator {
+
+    /**
+     * a method to check if something is prime
+     * uses the square root of the number to check if prime
+     * if a number
+     * @return
+     */
+    public static boolean isPrime(double number){
+        double root = Math.sqrt(number);
+        boolean isPrime=true;
+        for(double i=2; i<=root; i=i+1.0){
+            if(i%root==0){
+                isPrime = false;
+                break;
+
+            }
+            else{
+                isPrime = true;
+            }
+
+        }
+
+
+        return isPrime;
+    }
+
+
+
 }
---
+++ src/primes/PrimeTester.java
@@ -0,0 +1,9 @@
+package primes;
+
+public class PrimeTester {
+    public static void main(String[] args) {
+
+        System.out.println(PrimeNumberCalculator.isPrime(4));
+
+    }
+}
