fb8321935e896764f21c1a7fe5243f855a15b298
Gabriel Walsh <g.b.j.walsh@gmail.com>
2018-10-26 13:26:07 -0230

    generic commit message

--- src/primes/PrimeNumberCalculator.java
+++ src/primes/PrimeNumberCalculator.java
@@ -1,6 +1,8 @@
 package primes;
+
 /**
  * prime calculator class
+ *
  * @author Gabe Walsh
  */
 public class PrimeNumberCalculator {
@@ -9,18 +11,18 @@ public class PrimeNumberCalculator {
      * a method to check if something is prime
      * uses the square root of the number to check if prime
      * if a number
+     *
      * @return
      */
-    public static boolean isPrime(double number){
+    public static boolean isPrime(double number) {
         double root = Math.sqrt(number);
-        boolean isPrime=true;
-        for(double i=2; i<=root; i=i+1.0){
-            if(i%root==0){
+        boolean isPrime = true;
+        for (double i = 2; i <= root; i = i + 1.0) {
+            if (i % root == 0) {
                 isPrime = false;
                 break;
 
-            }
-            else{
+            } else {
                 isPrime = true;
             }
 
@@ -30,6 +32,38 @@ public class PrimeNumberCalculator {
         return isPrime;
     }
 
+    /**
+     * a method to get the number of primes between 0 and a number
+     * @param number
+     * @return
+     */
+
+    public static int numberOfPrimes(int number) {
+        int counter = 0;
+        for (double i = 0; i <= number; i++) {
+            if (isPrime(i)) {
+                /*System.out.printf("number%d is prime\n counter is%d ",i,counter);
+                counter = counter + 1;*/
+                System.out.printf("\nis prime %.2f returns ture",i);
+            }
+        }
+        return counter;
+    }
+
+    public static int[] primesList(int number) {
 
 
-}
+        int[] primeList = new int[numberOfPrimes(number)];
+        return primeList;
+    }
+
+
+
+
+
+
+public static String uniquePrimeFactorized(int number){
+
+        return"hi";
+        }
+        }
--- src/primes/PrimeTester.java
+++ src/primes/PrimeTester.java
@@ -1,9 +1,15 @@
 package primes;
 
+
 public class PrimeTester {
     public static void main(String[] args) {
+        System.out.println(PrimeNumberCalculator.isPrime(97));
+        System.out.println(PrimeNumberCalculator.isPrime(24));
 
-        System.out.println(PrimeNumberCalculator.isPrime(4));
+        System.out.println(PrimeNumberCalculator.numberOfPrimes(100));
 
+        System.out.println( PrimeNumberCalculator.uniquePrimeFactorized(5));
+
+        System.out.println(Math.PI*100);
     }
 }
