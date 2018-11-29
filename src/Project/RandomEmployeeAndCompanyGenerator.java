bf65ca47f7a1eac7f487aed3fa79e84906344a40
gbw767 <31829675+gbw767@users.noreply.github.com>
2018-11-29 19:26:21 -0330

    modified randomEmployeeGenerator

--- src/Project/RandomEmployeeAndCompanyGenerator.java
+++ src/Project/RandomEmployeeAndCompanyGenerator.java
@@ -1,13 +1,66 @@
 package Project;
 
+import employee.Employee;
+
+import java.util.List;
 import java.util.Random;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class RandomEmployeeAndCompanyGenerator {
+    String[] firstName = {
+            "Darrick",
+            "Troy",
+            "Lianne",
+            "Fidela",
+            "Doris",
+            "Chantell",
+            "Ashlee",
+            "Marylou",
+            "Lurlene",
+            "Elke",
+            "Al",
+            "Masako",
+            "Janelle",
+            "Jenice",
+            "Jadwiga",
+            "Ann",
+            "Liz",
+            "Lucilla",
+            "Joan",
+            "Danielle "};
+    String[] lastName = {
+            "Huttenback",
+            "Castillo",
+            "Benabou",
+            "Kee",
+            "Kenward",
+            "Ramey",
+            "Pulido",
+            "Lage",
+            "Middle",
+            "O'marley",
+            "Ogletree",
+            "Herrin",
+            "Clare",
+            "Isola",
+            "Cesar",
+            "Davy",
+            "Good",
+            "Diller",
+            "Grabbe",
+            "Vela-estrada"};
     /**
      * Salaryed employee generator
      * @param numberOSalaryedEmployees
      * @return
      */
+    public static List<Employee> generateEmployees() {
+        String[] firstName = {"Darrick", "Troy", "Lianne", "Fidela", "Doris", "Chantell", "Ashlee", "Marylou", "Lurlene", "Elke", "Al", "Masako", "Janelle", "Jenice", "Jadwiga", "Ann", "Liz", "Lucilla", "Joan", "Danielle "};
+        String[] lastName = {"Huttenback", "Castillo", "Benabou", "Kee", "Kenward", "Ramey", "Pulido", "Lage", "Middle", "O'marley", "Ogletree", "Herrin", "Clare", "Isola", "Cesar", "Davy", "Good", "Diller", "Grabbe", "Vela-estrada"};
+        Random rand = new Random();
+        List<Employee> listOfEmployees = new LinkedList<>();
     /*public SalaryedEmployee salaryedEmployeeGenerator(int numberOSalaryedEmployees ){
        for (int i = 0; i >= numberOSalaryedEmployees; i++) {
            SalaryedEmployee salaryedEmployee1 = new SalaryedEmployee("holder",
