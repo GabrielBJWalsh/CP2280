9ea065a49d3bd6dd1abc7642d8923d7b9a4b8451
gbw767 <31829675+gbw767@users.noreply.github.com>
2018-12-06 17:31:04 -0330

    made Insurance calculator ...eh

--- src/Project/EmployeeHeathInsureaceCalculator.java
+++ src/Project/EmployeeHeathInsureaceCalculator.java
@@ -1,4 +1,25 @@
 package Project;
 
 public class EmployeeHeathInsureaceCalculator {
+    private static final double BMI_OVER_25 = 1.75;
+    private static double BMI_OVER_30 = 2.5;
+    private static final double EXCEEDED_HEART_RATE_LIMIT = 1.5;
+
+    public static double calculateInsureance(double salary,
+                                             double bmi,
+                                             double heartRate,
+                                             double topHeartRate) {
+        double insuranceRate = 0;
+        insuranceRate += salary * .01;
+        if (bmi > 30) {
+            insuranceRate *= BMI_OVER_30;
+        } else if (bmi > 25) {
+            insuranceRate *= BMI_OVER_25;
+        }
+        if (heartRate > topHeartRate) {
+            insuranceRate *= EXCEEDED_HEART_RATE_LIMIT;
+        }
+        return insuranceRate;
+    }
+
 }
